d696706895f7d9319e92169c805dfc1d93b9b6cc
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-19 17:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstName = document.querySelector("#firstName").value;
+	let lastName = document.querySelector("#lastName").value;
+	alert(firstName + " " + lastName);
 }
